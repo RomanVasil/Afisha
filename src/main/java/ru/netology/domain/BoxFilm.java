706ec4d1774dfc4b807290db1d09526ae257490c
RomanVasil <jostik85@outlook.com>
2022-05-27 16:10:50 +0400

    version 3

--- src/main/java/ru/netology/domain/BoxFilm.java
+++ src/main/java/ru/netology/domain/BoxFilm.java
@@ -5,14 +5,44 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
 
 public class BoxFilm {
     private int id;
     private String filmName;
     private int year;
+
+    public BoxFilm(int id, String filmName, int year) {
+        this.id = id;
+        this.filmName = filmName;
+        this.year = year;
+    }
+
+//    public int getId() {
+//        return this.id;
+//    }
+//
+//    public String getFilmName() {
+//        return this.filmName;
+//    }
+//
+//    public int getYear() {
+//        return this.year;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setFilmName(String filmName) {
+//        this.filmName = filmName;
+//    }
+//
+//    public void setYear(int year) {
+//        this.year = year;
+//    }
 }
 
 
--- src/main/java/ru/netology/domain/PosterManager.java
+++ src/main/java/ru/netology/domain/PosterManager.java
@@ -3,41 +3,46 @@ package ru.netology.domain;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 
 public class PosterManager {
     private BoxFilm[] items = new BoxFilm[0];
     private int lastMaxCount = 10;
+
+    public PosterManager() {
+    }
 
     public PosterManager(int lastMaxCount) {
         this.lastMaxCount = lastMaxCount;
     }
 
     public void add(BoxFilm item) {
-        BoxFilm[] tmp = new BoxFilm[items.length + 1];
-        System.arraycopy(items, 0, tmp, 0, items.length);
+        BoxFilm[] tmp = new BoxFilm[this.items.length + 1];
+        System.arraycopy(this.items, 0, tmp, 0, this.items.length);
         tmp[tmp.length - 1] = item;
         this.items = tmp;
     }
 
     public BoxFilm[] findAll() {
-        int resultLength = Math.min(items.length, lastMaxCount);
-        BoxFilm[] result = new BoxFilm[resultLength];
-        for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
-            return this.items;
-        }
-        return result;
+//        int resultLength = Math.min(items.length, lastMaxCount);
+//        BoxFilm[] result = new BoxFilm[resultLength];
+//        for (int i = 0; i < result.length; i++) {
+//            int index = items.length - i - 1;
+//            result[i] = items[index];
+//            return this.items;
+//        }
+        return this.items;
     }
 
     public BoxFilm[] findLast() {
         int resultLength = Math.min(items.length, lastMaxCount);
         BoxFilm[] result = new BoxFilm[resultLength];
-        for (int i = 0; i < lastMaxCount; i++) {
-            result[i] = items[items.length - 1 - i];
+        for (int i = 0; i < this.lastMaxCount; i++) {
+            result[i] = this.items[this.items.length-1-i];
         }
+       // System.arraycopy(this.items, 0, result, 0, 1);
+
         return result;
     }
 }
